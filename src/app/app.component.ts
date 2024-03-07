186677dbd295176f97fa5285f0fec418fbc7c489
Prieto <pablojavierprietocepeda@gmail.com>
2024-03-07 12:03:51 -0300

    implementacion de cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import moment from 'moment';
+import { CardComponent } from './shared/components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'Funeraria Cubillos';
 
   fechaNacimiento = moment([1959, 3, 5]).format('YYYY-MM-DD');
