4c754a9442869d36bf28255817ac59a9caede68e
Prieto <pablojavierprietocepeda@gmail.com>
2024-03-10 17:55:40 -0300

    update servicios

--- src/app/servicios/funebres/funebres.component.ts
+++ src/app/servicios/funebres/funebres.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, Inject, Injectable, OnInit } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-funebres',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './funebres.component.html',
   styleUrl: './funebres.component.scss'
 })
-export class FunebresComponent {
+@Injectable()
+export class FunebresComponent implements AfterContentInit {
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngAfterContentInit() {
+    
+    var service = this.document.getElementById("service")!;
+    window.scrollTo(service.offsetTop, service.offsetTop);
+
+  }
 
 }
