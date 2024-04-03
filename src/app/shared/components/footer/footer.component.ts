46fd472c39821bfc7681c47d20a1346a216a17f2
Pablo Prieto Cepeda <pprieto.ice@ibm.com>
2024-04-03 13:06:48 -0300

    sombras

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
